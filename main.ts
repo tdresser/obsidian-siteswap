1f507921e22d697217a8e7cc237846b19955a9f8
Tim Dresser <tdresser@gmail.com>
2022-01-06 18:53:10 -0500

    Prettier

--- main.ts
+++ main.ts
@@ -1,38 +1,55 @@
-import { Editor, MarkdownView, Plugin, MarkdownPostProcessorContext, parseYaml } from 'obsidian';
-import { SiteswapSettingTab, SiteswapSettings, DEFAULT_SETTINGS } from 'settings';
+import {
+	Editor,
+	MarkdownView,
+	Plugin,
+	MarkdownPostProcessorContext,
+	parseYaml,
+} from "obsidian";
+import {
+	SiteswapSettingTab,
+	SiteswapSettings,
+	DEFAULT_SETTINGS,
+} from "settings";
+
+// TODO, add an attribute storing the current postprocessor, remove and reregister on settings update.
+// We don't need to pass in getSettings, just curry the settings object.
 
 export class SiteswapPlugin extends Plugin {
 	settings: SiteswapSettings;
 
 	static postprocessor = (getSettings: () => SiteswapSettings) => {
-		return (source:string, el: HTMLElement, ctx: MarkdownPostProcessorContext) => {
+		return (
+			source: string,
+			el: HTMLElement,
+			ctx: MarkdownPostProcessorContext
+		) => {
 			el.innerHTML = "";
-			let failure : string | null = null;
+			let failure: string | null = null;
 
-			let yaml : any = null;
+			let yaml: any = null;
 			try {
 				yaml = parseYaml(source.replaceAll(":", ": "));
-			} catch (e:any) {
+			} catch (e: any) {
 				failure = e.message;
 			}
 
 			if (failure != null) {
 				// Pass.
-			} else if (typeof(yaml) == "object") {
+			} else if (typeof yaml == "object") {
 				if (!("pattern" in yaml)) {
-					failure = 'Invalid siteswap: the "pattern" attribute is required.'
+					failure =
+						'Invalid siteswap: the "pattern" attribute is required.';
 				}
-			} else if (typeof(yaml) == "number" || typeof(yaml) == "string") {
+			} else if (typeof yaml == "number" || typeof yaml == "string") {
 				yaml = {
-					"pattern": "" + yaml
-				}
+					pattern: "" + yaml,
+				};
 			} else {
-				failure = "Invalid siteswap."
+				failure = "Invalid siteswap.";
 			}
 
-			
 			if (failure !== null) {
-				const message = document.createElement('p');
+				const message = document.createElement("p");
 				message.textContent = failure;
 				message.style.color = "#ff0000";
 				el.appendChild(message);
@@ -43,43 +60,52 @@ export class SiteswapPlugin extends Plugin {
 			console.log(paramsObject);
 
 			paramsObject["redirect"] = true;
-			const params = Object.keys(paramsObject).map(key => key + '=' + encodeURIComponent(paramsObject[key])).join(';')
+			const params = Object.keys(paramsObject)
+				.map((key) => key + "=" + encodeURIComponent(paramsObject[key]))
+				.join(";");
 
 			console.log(params);
 
-			const img = document.createElement('img');
+			const img = document.createElement("img");
 			img.src = "https://jugglinglab.org/anim?" + params;
 			img.style.width = "200px";
 			el.appendChild(img);
-	}
-}
+		};
+	};
 
 	async onload() {
 		await this.loadSettings();
 
-		console.log('loading siteswap plugin');
-		this.registerMarkdownCodeBlockProcessor("siteswap", SiteswapPlugin.postprocessor(() => this.settings));
+		console.log("loading siteswap plugin");
+		this.registerMarkdownCodeBlockProcessor(
+			"siteswap",
+			SiteswapPlugin.postprocessor(() => this.settings)
+		);
 
 		// This adds an editor command that can perform some operation on the current editor instance
 		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
+			id: "sample-editor-command",
+			name: "Sample editor command",
 			editorCallback: (editor: Editor, view: MarkdownView) => {
 				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});		
+				editor.replaceSelection("Sample Editor Command");
+			},
+		});
 
 		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SiteswapSettingTab(this.app, this));		
+		this.addSettingTab(new SiteswapSettingTab(this.app, this));
 	}
 
 	onunload() {
-		console.log('unloading siteswap plugin');
+		console.log("unloading siteswap plugin");
 	}
 
 	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
+		this.settings = Object.assign(
+			{},
+			DEFAULT_SETTINGS,
+			await this.loadData()
+		);
 	}
 
 	async saveSettings() {
--- settings.ts
+++ settings.ts
@@ -1,16 +1,16 @@
-import { SiteswapPlugin } from 'main';
-import { App, PluginSettingTab, Setting } from 'obsidian';
+import { SiteswapPlugin } from "main";
+import { App, PluginSettingTab, Setting } from "obsidian";
 
 export interface SiteswapSettings {
 	width: number;
 	height: number;
 	fps: number;
 	stereo: boolean;
-	slowdown:number;
-	border:number; 
+	slowdown: number;
+	border: number;
 	camangle: string;
 	showground: "auto" | "true" | "false";
-	hidejugglers:string;
+	hidejugglers: string;
 }
 
 export const DEFAULT_SETTINGS: SiteswapSettings = {
@@ -23,7 +23,7 @@ export const DEFAULT_SETTINGS: SiteswapSettings = {
 	camangle: "",
 	showground: "auto",
 	hidejugglers: "",
-}
+};
 
 export class SiteswapSettingTab extends PluginSettingTab {
 	plugin: SiteswapPlugin;
@@ -33,14 +33,14 @@ export class SiteswapSettingTab extends PluginSettingTab {
 		this.plugin = plugin;
 	}
 
-    // TODO: maybe write a declarative wrapper for this? Or refactor somehow.
-    // TODO: replace sliders with textfields that have validation.
+	// TODO: maybe write a declarative wrapper for this? Or refactor somehow.
+	// TODO: replace sliders with textfields that have validation.
 	display(): void {
-		const {containerEl} = this;
+		const { containerEl } = this;
 
 		containerEl.empty();
 
-		containerEl.createEl('h2', {text: 'Siteswap Settings'});
+		containerEl.createEl("h2", { text: "Siteswap Settings" });
 
 		const docs = document.createElement("div");
 		docs.innerHTML = `<p>Detailed documentation can be found at 
@@ -53,113 +53,141 @@ export class SiteswapSettingTab extends PluginSettingTab {
 
 		console.log(this.plugin.settings);
 
+		new Setting(containerEl)
+			.setName("width")
+			.setDesc("Width of the generated GIF, in pixels.")
+			.addSlider((slider) =>
+				slider
+					.setLimits(10, 2000, 10)
+					.setValue(this.plugin.settings.width)
+					.setDynamicTooltip()
+					.onChange(async (value) => {
+						this.plugin.settings.width = value;
+						await this.plugin.saveSettings();
+					})
+			);
 
 		new Setting(containerEl)
-			.setName('width')
-			.setDesc('Width of the generated GIF, in pixels.')
-			.addSlider(slider => slider
-				.setLimits(10, 2000, 10)
-				.setValue(this.plugin.settings.width)
-                .setDynamicTooltip()
-				.onChange(async (value) => {
-					this.plugin.settings.width = value;
-					await this.plugin.saveSettings();
-				}));
+			.setName("height")
+			.setDesc("Height of the generated GIF, in pixels.")
+			.addSlider((slider) =>
+				slider
+					.setLimits(10, 2000, 10)
+					.setValue(this.plugin.settings.height)
+					.setDynamicTooltip()
+					.onChange(async (value) => {
+						this.plugin.settings.height = value;
+						await this.plugin.saveSettings();
+					})
+			);
 
 		new Setting(containerEl)
-			.setName('height')
-			.setDesc('Height of the generated GIF, in pixels.')
-			.addSlider(slider => slider
-                .setLimits(10, 2000, 10)
-				.setValue(this.plugin.settings.height)
-				.setDynamicTooltip()
-				.onChange(async (value) => {
-					this.plugin.settings.height = value;
-					await this.plugin.saveSettings();
-				}));
+			.setName("fps")
+			.setDesc("Number of frames per second in the generated GIF.")
+			.addSlider((slider) =>
+				slider
+					.setLimits(1, 200, 1)
+					.setValue(this.plugin.settings.height)
+					.setDynamicTooltip()
+					.onChange(async (value) => {
+						this.plugin.settings.height = value;
+						await this.plugin.saveSettings();
+					})
+			);
 
 		new Setting(containerEl)
-			.setName('fps')
-			.setDesc('Number of frames per second in the generated GIF.')
-			.addSlider(slider => slider
-                .setLimits(1, 200, 1)
-				.setValue(this.plugin.settings.height)
-				.setDynamicTooltip()
-				.onChange(async (value) => {
-					this.plugin.settings.height = value;
-					await this.plugin.saveSettings();
-				}));
+			.setName("slowdown")
+			.setDesc(
+				"Defines an overall time slowdown factor (e.g., slowdown=1.0 is actual speed, slowdown=2.0 is half actual speed)."
+			)
+			.addSlider((slider) =>
+				slider
+					.setLimits(0.1, 10, 0.1)
+					.setValue(this.plugin.settings.slowdown)
+					.setDynamicTooltip()
+					.onChange(async (value) => {
+						this.plugin.settings.slowdown = value;
+						await this.plugin.saveSettings();
+					})
+			);
+
+		const showGroundOptions: Record<"auto" | "true" | "false", string> = {
+			true: "true",
+			false: "false",
+			auto: "auto",
+		};
+		new Setting(containerEl)
+			.setName("showground")
+			.setDesc("Whether to display the ground.")
+			.addDropdown((dropdown) =>
+				dropdown
+					.addOptions(showGroundOptions)
+					.setValue(this.plugin.settings.showground)
+					.onChange(async (value) => {
+						this.plugin.settings.showground = value as
+							| "auto"
+							| "true"
+							| "false";
+						await this.plugin.saveSettings();
+					})
+			);
 
 		new Setting(containerEl)
-			.setName('slowdown')
-			.setDesc('Defines an overall time slowdown factor (e.g., slowdown=1.0 is actual speed, slowdown=2.0 is half actual speed).')
-			.addSlider(slider => slider
-                .setLimits(0.1,10,0.1)
-				.setValue(this.plugin.settings.slowdown)
-				.setDynamicTooltip()
-				.onChange(async (value) => {
-					this.plugin.settings.slowdown = value;
-					await this.plugin.saveSettings();
-				}));
-
-		const showGroundOptions : Record<'auto' | 'true' | 'false', string> = {
-			true: 'true',
-			false: 'false',
-			auto: 'auto'
-		}
-		new Setting(containerEl)
-			.setName('showground')
-			.setDesc('Whether to display the ground.')
-			.addDropdown(dropdown => dropdown
-				.addOptions(showGroundOptions)
-				.setValue(this.plugin.settings.showground)
-				.onChange(async (value) => {
-					this.plugin.settings.showground = value as 'auto' | 'true' | 'false';
-					await this.plugin.saveSettings();
-				}));
+			.setName("border")
+			.setDesc("Defines a border (in pixels) around the animation.")
+			.addSlider((slider) =>
+				slider
+					.setLimits(0, 10, 1)
+					.setValue(this.plugin.settings.border)
+					.setDynamicTooltip()
+					.onChange(async (value) => {
+						this.plugin.settings.border = value;
+						await this.plugin.saveSettings();
+					})
+			);
 
 		new Setting(containerEl)
-			.setName('border')
-			.setDesc('Defines a border (in pixels) around the animation.')
-			.addSlider(slider => slider
-                .setLimits(0,10,1)
-				.setValue(this.plugin.settings.border)
-				.setDynamicTooltip()
-				.onChange(async (value) => {
-					this.plugin.settings.border = value;
-					await this.plugin.saveSettings();
-				}));
+			.setName("camangle")
+			.setDesc(
+				"Camera angles in degrees, given as one or a pair of angles. Example: camangle=(0,90). The first angle describes rotation of the camera around the juggler, and the second angle is the elevation angle given as degrees from directly overhead (i.e., 90 puts the camera on the same level as the juggler). Default value depends on the pattern."
+			)
+			.addText((text) =>
+				text
+					.setPlaceholder("(0,90)")
+					.setValue(this.plugin.settings.camangle)
+					.onChange(async (value) => {
+						this.plugin.settings.camangle = value;
+						await this.plugin.saveSettings();
+					})
+			);
 
 		new Setting(containerEl)
-			.setName('camangle')
-			.setDesc('Camera angles in degrees, given as one or a pair of angles. Example: camangle=(0,90). The first angle describes rotation of the camera around the juggler, and the second angle is the elevation angle given as degrees from directly overhead (i.e., 90 puts the camera on the same level as the juggler). Default value depends on the pattern.')
-			.addText(text => text
-				.setPlaceholder("(0,90)")
-				.setValue(this.plugin.settings.camangle)
-				.onChange(async (value) => {
-					this.plugin.settings.camangle = value;
-					await this.plugin.saveSettings();
-				}));
+			.setName("hidejugglers")
+			.setDesc(
+				"List of one or more jugglers to hide (i.e., not render) during animation. Examples: hidejugglers=1 or hidejugglers=(1,3)."
+			)
+			.addText((text) =>
+				text
+					.setPlaceholder("(1,3)")
+					.setValue(this.plugin.settings.hidejugglers)
+					.onChange(async (value) => {
+						this.plugin.settings.hidejugglers = value;
+						await this.plugin.saveSettings();
+					})
+			);
 
 		new Setting(containerEl)
-			.setName('hidejugglers')
-			.setDesc('List of one or more jugglers to hide (i.e., not render) during animation. Examples: hidejugglers=1 or hidejugglers=(1,3).')
-			.addText(text => text
-				.setPlaceholder("(1,3)")
-				.setValue(this.plugin.settings.hidejugglers)
-				.onChange(async (value) => {
-					this.plugin.settings.hidejugglers = value;
-					await this.plugin.saveSettings();
-				}));
-
-		new Setting(containerEl)
-			.setName('stereo')
-			.setDesc('Whether to display the pattern as a cross-eyed stereogram.')
-			.addToggle(toggle => toggle
-				.setValue(this.plugin.settings.stereo)
-				.onChange(async (value) => {
-					this.plugin.settings.stereo = value;
-					await this.plugin.saveSettings();
-				}));
-		}
+			.setName("stereo")
+			.setDesc(
+				"Whether to display the pattern as a cross-eyed stereogram."
+			)
+			.addToggle((toggle) =>
+				toggle
+					.setValue(this.plugin.settings.stereo)
+					.onChange(async (value) => {
+						this.plugin.settings.stereo = value;
+						await this.plugin.saveSettings();
+					})
+			);
+	}
 }
